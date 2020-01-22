b2dd9147bfddb3e75b5f8103fdd5724c0b79bb7a
Anirudh Gupta <anirudhgupta109@gmail.com>
2020-01-22 21:32:42 +0530

    themes: Exclude overlays in this repo from enforcing RRO
    Change-Id: I181a1263ab8871553243a72ffeffe5da59ef494d
    Signed-off-by: Anirudh Gupta <anirudhgupta109@gmail.com>

--- themes.mk
+++ themes.mk
@@ -39,3 +39,6 @@ PRODUCT_PACKAGES += \
 
 # Add theme specific overlays
 DEVICE_PACKAGE_OVERLAYS += vendor/themes/overlay/common
+
+# Allow overlays to be excluded from enforcing RRO
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/themes/overlay/common
